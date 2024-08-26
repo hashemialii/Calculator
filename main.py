4a46d0fc5499b0720a15f309181f98f8c57a9dc0
ali <alihashemi9675@gmail.com>
2024-08-26 16:01:29 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+class Calculator:
+
+    @staticmethod
+    def add(self, x, y):
+        return x + y
+
+    @staticmethod
+    def subtract(self, x, y):
+        return x - y
+
+    @staticmethod
+    def multiply(self, x, y):
+        return x * y
+
+    @staticmethod
+    def divide(self, x, y):
+        if y == 0:
+            return "Error! Division by zero."
+        else:
+            return x / y
+
+    def parse_input(self, user_input):
+
+        operators = {'+': self.add, '-': self.subtract, '*': self.multiply, '/': self.divide}
+        for op in operators:
+            if op in user_input:
+                num1, num2 = user_input.split(op)
+                try:
+                    num1, num2 = float(num1), float(num2)
+                    return operators[op](num1, num2)
+                except ValueError:
+                    return "Invalid input! Please enter valid numbers."
+        return "No valid operator found!"
+
+    def run(self):
+        print("Welcome to the Python Calculator!")
+
+        while True:
+            user_input = input("Enter your calculation: ")
+            result = self.parse_input(user_input)
+            print(f"The result is: {result}")
+
+            next_calculation = input("Do you want to perform another calculation? (yes/no): ").lower()
+            if next_calculation not in ['yes', 'y']:
+                break
+
+        print("Thank you for using the Python Calculator! Goodbye.")
+
+
+if __name__ == "__main__":
+
+    calc = Calculator()
+    calc.run()
+
